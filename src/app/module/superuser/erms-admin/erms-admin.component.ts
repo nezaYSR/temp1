5bbc70d7b097cf90b8f03a019acea610c861380c
Rez <moehamadrezha@gmail.com>
2022-06-07 22:36:15 +0700

    erms admin functionality

--- src/app/module/superuser/erms-admin/erms-admin.component.ts
+++ src/app/module/superuser/erms-admin/erms-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environments/environment";
+import {first, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-erms-admin',
@@ -6,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./erms-admin.component.scss']
 })
 export class ErmsAdminComponent implements OnInit {
+
+  userList = []
 
   breadcrumbs: {
     title: string
@@ -21,9 +26,29 @@ export class ErmsAdminComponent implements OnInit {
     }
   ]
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getUserList()
+  }
+
+  getUserList() {
+    return this.http.get<any>(`${environment.apiUrl}/superuser/userlist`)
+      .pipe(first())
+      .pipe(
+        map((resp) => {
+          if (resp) {
+            return resp.userList
+          }
+        })
+      )
+      .subscribe({
+        next: resp => {
+          this.userList = resp
+        },
+        error: err => {
+        }
+      })
   }
 
 }
