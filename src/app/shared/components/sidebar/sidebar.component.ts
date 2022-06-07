ca1134f0a520ffbb60b643515f81ca05710ea2b8
Rez <moehamadrezha@gmail.com>
2022-06-07 20:12:00 +0700

    configure dynamic shared sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -9,6 +9,72 @@ import {Router} from "@angular/router";
 })
 export class SidebarComponent implements OnInit {
 
+  sidebarData = [
+    {
+      "title": "ERMS Admin",
+      "fullPath": "/superuser/ErmsAdmin",
+      "icon": "fas fa-user-friends",
+      "roles": [
+        "super_user"
+      ],
+      "child": []
+    },
+    {
+      "title": "Risk Admin Task",
+      "fullPath": "/superuser/RiskAdminTask",
+      "icon": "fas fa-user-cog",
+      "roles": [
+        "super_user"
+      ],
+      "child": [
+        {
+          "title": "Task",
+          "fullPath": "/superuser/home",
+          "icon": "far fa-circle",
+          "roles": [
+            "super_user"
+          ]
+        }
+      ]
+    },
+    {
+      "title": "User Admin Task",
+      "fullPath": "/superuser/UserAdminTask",
+      "icon": "fas fa-user-shield",
+      "roles": [
+        "super_user"
+      ],
+      "child": [
+        {
+          "title": "Task",
+          "fullPath": "/superuser/home",
+          "icon": "far fa-circle",
+          "roles": [
+            "super_user"
+          ]
+        }
+      ]
+    },
+    {
+      "title": "Activities Log",
+      "fullPath": "/erms/ActivitiesLog",
+      "icon": "fas fa-scroll",
+      "roles": [
+        "super_user"
+      ],
+      "child": []
+    },
+    {
+      "title": "Submit Log",
+      "fullPath": "/erms/SubmitLog",
+      "icon": "fas fa-file",
+      "roles": [
+        "super_user"
+      ],
+      "child": []
+    }
+  ]
+
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService
