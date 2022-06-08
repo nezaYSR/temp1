214a142720bee79d3f15e2a6a2e79f6072cc3d18
Rez <moehamadrezha@gmail.com>
2022-06-08 16:26:41 +0700

    implement live search & ngx component destroy package

--- src/app/module/superuser/erms-admin/erms-admin.component.ts
+++ src/app/module/superuser/erms-admin/erms-admin.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../../environments/environment";
-import {first, map} from "rxjs/operators";
+import {debounceTime, distinctUntilChanged, first, map, takeUntil} from "rxjs/operators";
+import {Subject} from "rxjs";
+import {componentDestroyed, OnDestroyMixin} from "@w11k/ngx-componentdestroyed";
 
 @Component({
   selector: 'app-erms-admin',
   templateUrl: './erms-admin.component.html',
   styleUrls: ['./erms-admin.component.scss']
 })
-export class ErmsAdminComponent implements OnInit {
-
+export class ErmsAdminComponent extends OnDestroyMixin implements OnInit {
+  private searchTermSubject = new Subject<string>()
+  private inputListener = this.searchTermSubject.pipe(
+    debounceTime(500),
+    distinctUntilChanged()
+  );
   userList = []
 
   breadcrumbs: {
@@ -26,10 +32,40 @@ export class ErmsAdminComponent implements OnInit {
     }
   ]
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    super();
+  }
 
   ngOnInit(): void {
     this.getUserList()
+
+    this.inputListener
+      .pipe(takeUntil(componentDestroyed(this)))
+      .subscribe(currentValue => {
+      if(currentValue === ''){
+        this.getUserList()
+      }else{
+        this.http.get<any>(`${environment.apiUrl}/riskadm/finduser`, {
+          params:{
+            // TODO: sesuaikan lagi dengan Backend
+            term: currentValue
+          }
+        })
+          .pipe(first())
+          .pipe(
+            map((resp) => {
+              if (resp) {
+                return resp.userFound
+              }
+            })
+          )
+          .subscribe({
+            next: resp => {
+              this.userList = resp
+            }
+          })
+      }
+    });
   }
 
   getUserList() {
@@ -45,10 +81,16 @@ export class ErmsAdminComponent implements OnInit {
       .subscribe({
         next: resp => {
           this.userList = resp
-        },
-        error: err => {
         }
       })
+  }
+
+  getInputValue(event: Event): string {
+    return (event.target as HTMLInputElement).value;
+  }
+
+  searchUser(searchTerm: string) {
+    this.searchTermSubject.next(searchTerm);
   }
 
 }
